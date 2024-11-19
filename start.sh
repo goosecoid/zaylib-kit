2c7d4bf99b475a82b7f33e884f83ce88d1ecd117
goosecoid <adblavier@gmail.com>
2024-11-19 09:27:40 +0100

    add .gitignore

--- start.sh
+++ start.sh
@@ -20,6 +20,38 @@ mkdir "$PROJECT_NAME"
 
 cd "$PROJECT_NAME"
 
+read -r -d '' GITIGNORE <<EOF
+
+# This file is for zig-specific build artifacts.
+# If you have OS-specific or editor-specific files to ignore,
+# such as *.swp or .DS_Store, put those in your global
+# ~/.gitignore and put this in your ~/.gitconfig:
+#
+# [core]
+#     excludesfile = ~/.gitignore
+#
+# Cheers!
+# -andrewrk
+
+.zig-cache/
+zig-out/
+/release/
+/debug/
+/build/
+/build-*/
+/docgen_tmp/
+
+# Although this was renamed to .zig-cache, let's leave it here for a few
+# releases to make it less annoying to work with multiple branches.
+zig-cache/
+
+# Optional
+# raylib/
+
+EOF
+
+echo "$GITIGNORE" >.gitignore
+
 read -r -d '' BUILD_FILE <<EOF
 
 const std = @import("std");
